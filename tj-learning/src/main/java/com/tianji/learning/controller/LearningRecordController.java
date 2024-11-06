1407167a825cdcb9341c306e0026f7ad4d6bb452
ldn <attentionlhy@163.com>
2024-11-06 22:17:37 +0800

    生成学习记录基本代码

---
+++ tj-learning/src/main/java/com/tianji/learning/controller/LearningRecordController.java
@@ -0,0 +1,20 @@
+package com.tianji.learning.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 学习记录表 前端控制器
+ * </p>
+ *
+ * @author 鲁小牛  
+ * @since 2024-11-06
+ */
+@RestController
+@RequestMapping("/learning-record")
+public class LearningRecordController {
+
+}
