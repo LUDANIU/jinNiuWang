7ccdbdc1ac060f9121d144ab0f9de563a72ad988
ldn <attentionlhy@163.com>
2024-12-05 21:52:18 +0800

    点赞业务(代码生成2)

---
+++ tj-remark/src/main/java/com/tianji/remark/controller/LikedRecordController.java
@@ -0,0 +1,20 @@
+package com.tianji.remark.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 点赞记录表 前端控制器
+ * </p>
+ *
+ * @author ldn
+ * @since 2024-12-05
+ */
+@RestController
+@RequestMapping("/liked-record")
+public class LikedRecordController {
+
+}
---
+++ tj-remark/src/main/java/com/tianji/remark/service/ILikedRecordService.java
@@ -0,0 +1,16 @@
+package com.tianji.remark.service;
+
+import com.tianji.remark.domain.po.LikedRecord;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 点赞记录表 服务类
+ * </p>
+ *
+ * @author ldn
+ * @since 2024-12-05
+ */
+public interface ILikedRecordService extends IService<LikedRecord> {
+
+}
