31bf91dbbfe96f4242ed80cb92d48a7f10002f11
ldn <attentionlhy@163.com>
2024-12-09 14:29:03 +0800

    积分系统代码生成

---
+++ tj-learning/src/main/java/com/tianji/learning/service/Impl/PointsBoardServiceImpl.java
@@ -0,0 +1,20 @@
+package com.tianji.learning.service.impl;
+
+import com.tianji.learning.domain.po.PointsBoard;
+import com.tianji.learning.mapper.PointsBoardMapper;
+import com.tianji.learning.service.IPointsBoardService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 学霸天梯榜 服务实现类
+ * </p>
+ *
+ * @author ldn
+ * @since 2024-12-09
+ */
+@Service
+public class PointsBoardServiceImpl extends ServiceImpl<PointsBoardMapper, PointsBoard> implements IPointsBoardService {
+
+}
