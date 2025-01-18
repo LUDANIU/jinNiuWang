db2c72b36584713eb42e4271b98676bfaf81446b
haotian Lu <attentionlhy@163.com>
2025-01-18 14:31:07 +0800

    usercoupon代码生成（7）

---
+++ tj-promotion/src/main/java/com/tianji/promotion/controller/UserCouponController.java
@@ -0,0 +1,20 @@
+package com.tianji.promotion.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 用户领取优惠券的记录，是真正使用的优惠券信息 前端控制器
+ * </p>
+ *
+ * @author 小牛
+ * @since 2025-01-18
+ */
+@Controller
+@RequestMapping("/user-coupon")
+public class UserCouponController {
+
+}
